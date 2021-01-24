c33430574c9939abb238eb02ca1c7653d13d8a8e
tamara ostapets <tamara.ostapec@gmail.com>
2021-01-24 18:04:41 +0200

    Initial commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const menu = document.querySelector('.nav');
+
+const toggler = document.querySelector('.toggler');
+
+const togglerLine = document.querySelector('.toggler__line');
+
+toggler.onclick = function toggleSidebar() {
+  menu.classList.toggle('nav--open');
+  toggler.classList.toggle('toggler--active');
+  togglerLine.classList.toggle('toggler__line--active');
+};
